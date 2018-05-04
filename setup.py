a03152e315f1628541d672818880068b616edad9
Shiyam S <austin.shyam@gmail.com>
2018-05-04 19:26:37 +0530

    create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ObservableCollections',
+    version='0.0.1',
+    description='Rx based collections that supports change notification',
+    author='Shiyam S',
+    author_email='austin.shyam@gmail.com',
+    license='MIT',
+    download_url='https://github.com/shyam-s00/observablelist.py',
+
+    packages=['reactive']
+)
